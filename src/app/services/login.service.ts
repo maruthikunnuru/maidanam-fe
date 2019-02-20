cf3b77d87b0809ef77d6ae8cc462914fce0ace08
Vinod Aripaka <vinod.aripaka@ge.com>
2019-02-19 23:33:25 -0600

    Login Service Update

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,20 +1,43 @@
-import {AuthService, GoogleLoginProvider, SocialUser} from 'angularx-social-login';
 import {Injectable} from '@angular/core';
+import {SocialUser} from "angularx-social-login";
+import {UserModel} from "../shared/user.model";
 
 @Injectable()
 export class LoginService {
 
-    user: SocialUser;
-    loggedIn: boolean;
+    user: UserModel;
+    private users: string[] = [
+        'vinod.aripaka@gmail.com',
+        'alurisankar@gmail.com'
+    ];
 
-    constructor(private authService: AuthService) {}
+    constructor() { }
 
-    signInWithGoogle(): void {
-        this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    isRegisteredUser(username: string) {
+        if (this.users.includes(username)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    signOut(): void {
-        this.authService.signOut();
+    setUserProfile(socialUser: SocialUser) {
+        this.user = new UserModel(socialUser.name,
+            socialUser.firstName,
+            socialUser.lastName,
+            socialUser.email,
+            socialUser.photoUrl, []);
     }
 
+    getUserProfile() {
+        return this.user;
+    }
+
+    registerUser(username: string) {
+        this.users.push(username);
+    }
+
+    getRegisteredUsers() {
+        return this.users.slice();
+    }
 }
