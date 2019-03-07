0fecce69e91b701d4235a3d44f2151d4a8fb1245
Vinod Aripaka <vinod.aripaka@ge.com>
2019-03-07 11:50:28 -0600

    Changes Login Service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Router} from '@angular/router';
 import {LoginService} from '../z-services/login.service';
+import {UserModel} from "../z-models/user.model";
 
 @Component({
   selector: 'app-register',
@@ -13,7 +14,7 @@ export class RegisterComponent implements OnInit {
   @ViewChild('r') loginForm: NgForm;
 
 
-  username: string;
+  user: UserModel;
   registeredUsers: string[];
 
   constructor(private router: Router,
@@ -28,14 +29,14 @@ export class RegisterComponent implements OnInit {
 
     if(referralCode === '12345') {
 
-      this.username = this.loginService.getUserProfile().emailId;
+      this.user = this.loginService.getUserProfile();
       this.registeredUsers = this.loginService.getRegisteredUsers();
 
-      if (this.registeredUsers.includes(this.username)) {
+      if (this.registeredUsers.includes(this.user.userName)) {
         alert('You are already a registered User');
 
       } else {
-        this.loginService.registerUser(this.username);
+        this.loginService.registerUser(this.user.userName, this.user.firstName, this.user.lastName, '12345');
         alert('You are successfully registered into Maidanam');
       }
       this.router.navigate(['/home']);
