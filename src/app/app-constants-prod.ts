8578bf5353d0403452077511da043bf5e04f4982
Vinod Aripaka <vinod.aripaka@ge.com>
2019-05-28 23:11:51 -0500

    appconstants prod file created

---
+++ src/app/app-constants-prod.ts
@@ -0,0 +1,7 @@
+export class AppConstants {
+    // public static API_ENDPOINT = 'http://localhost:8090';
+    // public static GOOGLE_KEY = '515718912201-5iisnrp83m8d31t1l80c898h3g5v22n4.apps.googleusercontent.com';
+
+    public static API_ENDPOINT = 'http://maidanam.in:8081';
+    public static GOOGLE_KEY = '822418301367-5vrrcuctnl4ovk25q38qrdqs4ggvbh8r.apps.googleusercontent.com';
+}
