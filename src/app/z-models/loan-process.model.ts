28c2fddf23e250e7eae54a33cab27618476634a1
Vinod Aripaka <vinod.aripaka@ge.com>
2019-06-21 22:11:46 -0500

    Loan Process Integration in Admin Services

--- src/app/z-models/loan-process.model.ts
+++ src/app/z-models/loan-process.model.ts
@@ -1,10 +1,12 @@
 export class LoanProcessModel {
-    public loanRefCode: string;
+    private _loanRefCode: string;
+    public loanAmount: number;
     public adminToken: string;
 
 
-    constructor(loanRefCode: string, adminToken: string) {
-        this.loanRefCode = loanRefCode;
+    constructor(loanRefCode: string, loanAmount: number, adminToken: string) {
+        this._loanRefCode = loanRefCode;
+        this.loanAmount = loanAmount;
         this.adminToken = adminToken;
     }
 }
